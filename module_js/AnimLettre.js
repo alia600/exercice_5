895495c8097bece0c37c49077e8d9c1ff4d5de07
= <catherinebr24@gmail.com>
2019-02-08 17:24:53 -0500

    ajout du constructeur

--- module_js/AnimLettre.js
+++ module_js/AnimLettre.js
@@ -6,10 +6,22 @@ export class AnimLettre {
 
     /**
      * Classe permettant de créer et d'animer une introduction
-     * @param {object} o - contient l'ensemble des mots d'intro
+     * @param {string} lesLettres - contient l'ensemble des mots d'intro
      * @param {DOMElement} elementParent - Conteneur de l'animation
      * @param {function} fonction - l'adresse de la fonction à exécuter après l'animation
          
      }}
      */
+
+    constructor(lesLettres, elementParent, fonction) {
+        //Récupérer les valeurs passées en paramètre	
+
+        this.elmParent = elementParent
+        this.AnimLettre()
+        this.fonction = fonction
     }
+
+    AnimLettre(){
+        
+    }
+}
