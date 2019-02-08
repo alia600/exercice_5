c9004a10b085cae81c600c318ce3abb86fbe3a55
= <catherinebr24@gmail.com>
2019-02-08 17:21:53 -0500

    création de la calsse AnimLettre

---
+++ module_js/AnimLettre.js
@@ -0,0 +1,15 @@
+import {
+    Util
+} from './Util.js'
+
+export class AnimLettre {
+
+    /**
+     * Classe permettant de créer et d'animer une introduction
+     * @param {object} o - contient l'ensemble des mots d'intro
+     * @param {DOMElement} elementParent - Conteneur de l'animation
+     * @param {function} fonction - l'adresse de la fonction à exécuter après l'animation
+         
+     }}
+     */
+    }
